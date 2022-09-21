ed52902e6ee52e822afe7605b14169a035eb4539
Zaidi <zaidiahsan.r@gmail.com>
2022-09-21 17:39:45 -0500

    User controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -49,4 +49,67 @@ const userController = {
                 res.status(400).json(err)
             });
     },
+
+    //Delete a User
+    deleteUserById({ params }, res) {
+        User.findOneAndDelete({ _id: params.id })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    return res.status(404).json({ message: 'No User found with this id!' });
+                }
+                User.updateMany(
+                    { _id: { $in: dbUserData.friends } },
+                    { $pull: { friends: params.id } }
+                )
+                    .then(() => {
+                        Thought.deleteMany({ username: dbUserData.username })
+                            .then(() => {
+                                res.json({ message: "Successfully Deleted User" });
+                            })
+                            .catch((err) => res.status(400).json(err));
+                    })
+                    .catch((err) => res.status(400).json(err));
+            })   
+            .catch((err) => res.status(400).json(err));
+    },
+
+    //add a new friend to User friend list
+    addFriendstoList({ params }, res) {
+        User.findOneAndUpdate(
+            { _id: params.userId },
+            {$addToSet: { friends: params.friendId } },
+            { new: true, runValidators: true }
+        )
+        .then(dbUserData => {
+            if (!dbUserData) {
+                return res.status(404).json({ message: 'No User found with this id!' });
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err)
+        });
+    },
+
+    //Delete friend from User friend list
+    removeFriend({ params }, res) {
+        User.findOneAndUpdate(
+            { _id: params.userId },
+            { $pull: { friends: params.friendId} },
+            { new: true, runValidators: true}
+        )
+        .then(dbUserData => {
+            if (!dbUserData) {
+                return res.status(404).json({ message: 'No User found with this id!' });
+            }
+            res.json({ message: "Successfully removed friend" });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err)
+        });
+    }
 }
+
+module.exports = userController;
